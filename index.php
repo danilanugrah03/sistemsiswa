979445af43bee70b07de6a5de742ce138d1ce367
danilanugrah03 <danilanugrah01@gmail.com>
2021-11-20 11:44:08 +0700

    all of title on the sql has been chaanged from index

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
         if($hasil->num_rows > 0){
             while($row= $hasil->fetch_assoc()){
-                echo $row['Nama_Siswa']. "====". $row["Nim"]. "====". $row["Tanggal_Lahir"]. "====". $row["Tempat_Lahir"]. "<br>";
+                echo $row['nama_siswa']. "====". $row["nim"]. "====". $row["tanggal_lahir"]. "====". $row["tempat_lahir"]. "<br>";
 
             }
         }
